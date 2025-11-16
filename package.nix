d60fab82d1d628c056f57943cfaf83888e9280c7
xam <m.porseryd@gmail.com>
2025-11-16 14:14:29 +0100

    working without sandbox

--- package.nix
+++ package.nix
@@ -5,59 +5,72 @@
 , nodejs
 , zulu25
 , makeWrapper
-, nginx
+, buildNpmPackage
 }:
+
+let
+  src = ./.;
+
+  frontend = buildNpmPackage {
+    pname = "booklore-ui";
+    version = "0.0.40";
+   
+    src = ./booklore-ui;
+    
+    # to get this hash, run: nix-shell -p prefetch-npm-deps --run "prefetch-npm-deps ./booklore-ui/package-lock.json"
+    npmDepsHash = "sha256-QIylVk2Q13ilatrpWD6oWYPzMA2SOIFUPzRajJuHi+w=";
+
+    npmBuildScript = "build";
+    npmBuildFlags = [ "--" "--configuration=production" ];
+
+    installPhase = ''
+      runHook preInstall
+      
+      mkdir -p $out
+      cp -r dist/booklore/* $out/
+      
+      runHook postInstall
+    '';
+  };
+
+in
 
 stdenv.mkDerivation rec {
   pname = "booklore";
   version = "0.0.40";
+  inherit src;
 
-  src = ./.;
-
-  nativeBuildInputs = [
-    gradle
-    nodejs
-    zulu25
-    makeWrapper
-  ];
+  nativeBuildInputs = [ gradle zulu25 makeWrapper ];
 
   buildPhase = ''
-    export GRADLE_USER_HOME=$TMPDIR/gradle
-    export HOME=$TMPDIR
-    export npm_config_cache=$TMPDIR/npm-cache
-    
-    # Build Angular frontend
-    echo "building frontend"
-    cd booklore-ui
-    npm config set registry http://registry.npmjs.org/
-    npm install --force 
-    patchShebangs node_modules
-    npm run build --configuration=production
-    cd ..
-    
-    # Build Spring Boot backend
+    runHook preBuild
+
+    echo "Building backend..."
+    export GRADLE_USER_HOME=$(mktemp -d)
     cd booklore-api
     chmod +x gradlew
-    ./gradlew clean build -x test --no-daemon
+    ./gradlew build -x test --no-daemon
     cd ..
+
+    runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/{bin,lib,share/booklore/static}
     
-    # Install the Spring Boot JAR
-    cp booklore-api/build/libs/*.jar $out/lib/
+    cp booklore-api/build/libs/*-SNAPSHOT.jar $out/lib/
     
-    # Install the frontend
-    cp -r booklore-ui/dist/booklore/* $out/share/booklore/static/
+    cp -r ${frontend}/* $out/share/booklore/static/
     
-    # Create executable
+    JAR_FILE=$(ls booklore-api/build/libs/*-SNAPSHOT.jar | head -n1)
+    JAR_NAME=$(basename "$JAR_FILE")
     makeWrapper ${zulu25}/bin/java $out/bin/booklore \
-      --add-flags "-jar $out/lib/booklore-api.jar"
+      --add-flags "-jar $out/lib/$JAR_NAME"
+    
+    runHook postInstall
   '';
 
-
-  outputHashMode = "recursive";
-  outputHashAlgo = "sha256";
-  outputHash = lib.fakeSha256;
+    # to allow network access, temporary since it doesnt allow sandbox
+  __noChroot = true;
 }
